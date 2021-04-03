da8dd5c72f9a04630d7aa6fea51ed81f2eca526d
GhzGarage <57848836+GhzGarage@users.noreply.github.com>
2021-04-03 01:08:46 -0500

    fxmanifest

--- qb-multicharacter/fxmanifest.lua
+++ qb-multicharacter/fxmanifest.lua
@@ -1,13 +1,17 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'cerulean'
+game 'gta5'
+
+description 'QB-Multicharacter'
+version '1.0.0'
 
 ui_page "html/index.html"
 
 client_scripts {
-    'client/main.lua',
+    'client/main.lua'
 }
 
 server_scripts {
-    'server/main.lua',
+    'server/main.lua'
 }
 
 files {
@@ -16,4 +20,9 @@ files {
     'html/reset.css',
     'html/script.js',
     'html/qbus-logo.png'
+}
+
+dependencies {
+    'qb-core',
+    'qb-spawn'
 }
