089edac9c35d809f1448c8c18eea67b6e5ff0099
Slug <81391361+SlugTeamSix@users.noreply.github.com>
2021-08-25 21:22:44 -0400

    Update script.js
    Fixed typo in character selection screen

--- html/script.js
+++ html/script.js
@@ -102,7 +102,7 @@ function setupCharInfo(cData) {
         '<div class="character-info-box"><span id="info-label">Job: </span><span class="char-info-js">'+cData.job.label+'</span></div>' +
         '<div class="character-info-box"><span id="info-label">Cash: </span><span class="char-info-js">&#36; '+cData.money.cash+'</span></div>' +
         '<div class="character-info-box"><span id="info-label">Bank: </span><span class="char-info-js">&#36; '+cData.money.bank+'</span></div>' +
-        '<div class="character-info-box"><span id="info-label">Phone nummber: </span><span class="char-info-js">'+cData.charinfo.phone+'</span></div>' +
+        '<div class="character-info-box"><span id="info-label">Phone number: </span><span class="char-info-js">'+cData.charinfo.phone+'</span></div>' +
         '<div class="character-info-box"><span id="info-label">Account number: </span><span class="char-info-js">'+cData.charinfo.account+'</span></div>');
     }
 }
