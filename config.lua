7f7ecb8130618999e3e147cc9b10e903cc994bf5
GhzGarage <gigahertzgarage@mail.com>
2021-10-01 01:10:49 -0500

    enable apartment default

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 Config = {}
-Config.StartingApartment = false -- Enable/disable starting apartments (make sure to set default spawn coords)
+Config.StartingApartment = true -- Enable/disable starting apartments (make sure to set default spawn coords)
 Config.Interior = vector3(-814.89, 181.95, 76.85) -- Interior to load where characters are previewed
 Config.DefaultSpawn = vector3(-1035.71, -2731.87, 12.86) -- Default spawn coords if you have start apartments disabled
 Config.PedCoords = vector4(-813.97, 176.22, 76.74, -7.5) -- Create preview ped at these coordinates
