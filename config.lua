2ad252ad4fbaff1371cb1dbe23b8bd28e2f91f72
izMystic <mystic@izmystic.dev>
2021-10-04 22:07:33 -0400

    fix(config.lua): correct cam coords

--- config.lua
+++ config.lua
@@ -4,4 +4,4 @@ Config.Interior = vector3(-814.89, 181.95, 76.85) -- Interior to load where char
 Config.DefaultSpawn = vector3(-1035.71, -2731.87, 12.86) -- Default spawn coords if you have start apartments disabled
 Config.PedCoords = vector4(-813.97, 176.22, 76.74, -7.5) -- Create preview ped at these coordinates
 Config.HiddenCoords = vector4(-812.23, 182.54, 76.74, 156.5) -- Hides your actual ped while you are in selection
-Config.CamCoords = vector4(-814.02, 179.56, 76.74, 198.5) -- Camera coordinates for character preview screen
+Config.CamCoords = vector4(-813.46, 178.95, 76.85, 174.5) -- Camera coordinates for character preview screen
