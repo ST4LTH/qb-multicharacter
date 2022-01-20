9211fcee7811076c93b2a26140bd689bb7bef58a
KUMApt <coelhodaniel12@gmail.com>
2022-01-20 16:45:42 +0000

    Set default number of characters per player
    Added support for the player to set the default number of characters per player and per rockstar license

--- config.lua
+++ config.lua
@@ -5,3 +5,8 @@ Config.DefaultSpawn = vector3(-1035.71, -2731.87, 12.86) -- Default spawn coords
 Config.PedCoords = vector4(-813.97, 176.22, 76.74, -7.5) -- Create preview ped at these coordinates
 Config.HiddenCoords = vector4(-812.23, 182.54, 76.74, 156.5) -- Hides your actual ped while you are in selection
 Config.CamCoords = vector4(-813.46, 178.95, 76.85, 174.5) -- Camera coordinates for character preview screen
+
+Config.DefaultNumberOfCharacters = 1 -- Define maximum amount of default characters (maximum 5 characters defined by default)
+Config.PlayersNumberOfCharacters = { -- Define maximum amount of player characters by rockstar license (you can find this license in your server's database in the player table)
+    { license = "license:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", numberOfChars = 2 },
+}
